a0b076991ccab156080e9871a239bb728be31c56
Laura Gosney <lauragosney82@gmail.com>
2017-11-14 00:53:17 +0000

    user system and bookmarks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before any page loads, find the current_user
+  before_action :find_current_user
+
+  # add in method to use in the views
+
+  helper_method :is_logged_in?
+
+  def find_current_user
+
+    if session[:user_id].present?
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+
+  end
+
+  # check login status
+  def check_login
+    unless is_logged_in?
+      # all cool
+      redirect_to new_session_path
+    end
+  end
+
+  # is the person logeed in
+
+  def is_logged_in?
+    session[:user_id].present?
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,8 @@
 class ReviewsController < ApplicationController
 
+  # check if logged in
+  before_action :check_login, except: [:index, :show]
+
   def index
     #this is our list page for our reviews
     @price = params[:price]
@@ -37,6 +40,9 @@ class ReviewsController < ApplicationController
     # take info from the form and add it to the model
     @review = Review.new(form_params)
 
+    # and then associate it with a user
+    @review.user = @current_user
+
     # we want to check if the model can be saved
     # if it is, we're going to the homepage again
     # if it is'nt, show the new review form
@@ -59,8 +65,10 @@ class ReviewsController < ApplicationController
     # find the individual review
     @review = Review.find(params[:id])
 
-    # destroy
-    @review.destroy
+    # destroy if they have access
+    if @review.user == @current_user
+      @review.destroy
+    end
 
     # redirect to the homepage
     redirect_to root_path
@@ -72,6 +80,9 @@ class ReviewsController < ApplicationController
     # find individual review to edit
     @review = Review.find(params[:id])
 
+    if @review.user != @current_user
+      redirect_to root_path
+    end
   end
 
 
@@ -80,20 +91,23 @@ class ReviewsController < ApplicationController
     # find the individual review
     @review = Review.find(params[:id])
 
-    # update with the new information from the form
-    if @review.update(form_params)
 
-
-    # redirect somewhere new
-    redirect_to review_path(@review)
+    if @review.user != @current_user
+      redirect_to root_path
     else
-      render "edit"
-    end
+      # update with the new information from the form
+      if @review.update(form_params)
 
+        # redirect somewhere new
+        redirect_to review_path(@review)
+      else
+        render "edit"
+      end
+    end
   end
 
   def form_params
-  params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance, :cuisine, :price, :address)
+    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance, :cuisine, :price, :address)
   end
 
 end
